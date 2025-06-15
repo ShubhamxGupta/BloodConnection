a8ba031be1fe948fb2ea0679f7d334455d95990d
Shubham Gupta <shubhamxgupta1@gmail.com>
2025-06-15 12:38:04 +0530

    hosting

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: import.meta.env.VITE_API_URL || 'https://backend-phhh9hy5j-shubhamxgupta.vercel.app'
+    API_URL: import.meta.env.VITE_API_URL || 'https://backend-ovijbfsvk-shubhamxgupta.vercel.app'
 };
 
 export default config; 
